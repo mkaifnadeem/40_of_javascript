8cf089e45d512caf8b2a2068803d6726063c526d
mkaifnadeem <mkaifnadeem024@gmail.com>
2025-06-14 17:44:08 +0500

    fix:correct the head tage typeo in Day 1

---
+++ Day 6/solution.js
@@ -0,0 +1,26 @@
+//// Task 1:Write a Function to Convert Celsius to Fahrenheit
+
+function celsiusToFahrenheit(celsius) {
+  let fahrenheit = (celsius * 9) / 5 + 32;
+  return fahrenheit;
+}
+
+let tempInFahrenheit = celsiusToFahrenheit;
+
+console.log(tempInFahrenheit(45));
+
+//// Task 2:Create a Function to Find the Maximum of Two Numbers
+
+function findMax(num1, num2) {
+  if (num1 > num2) {
+    console.log(`${num1} is grater then ${num2}`);
+  } else {
+    console.log(`${num2} is grater then ${num1}`);
+  }
+}
+
+findMax(22, -33);
+
+//// Task 3:Function to Check if a String is a Palindrome
+
+,                       
